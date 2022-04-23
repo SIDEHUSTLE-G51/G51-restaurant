81f6deac50333bddda68ff20523d14b3e48fe4cf
designeralabi <bdlkrmalabi@gmail.com>
2022-04-23 21:57:12 +0100

    remove homePage Component and home from route text

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,32 +1,31 @@
-import React from 'react'
-import {Nav, NavLink, Bars, NavMenu } from './Navbar'
+import React from "react";
+import { Nav, NavLink, Bars, NavMenu } from "./Navbar";
 
 const Navbar = () => {
   return (
-      <>
-        <Nav>
-            <NavLink to="/">
-                <img src={require('../../images/logo.png')} alt='logo'/>
-            </NavLink>
-            <Bars />
-            <NavMenu>
-                <NavLink to="/home">
-                    <h3>Home</h3>
-                </NavLink>
-                <NavLink to="/about">
-                    <h3>About</h3>
-                </NavLink>
-                <NavLink to="/products">
-                    <h3>Products</h3>
-                </NavLink>
-                <NavLink to="/contact">
-                    <h3>Contact</h3>
-                </NavLink>
-
-            </NavMenu>
-        </Nav>
-      </>
+    <>
+      <Nav>
+        <NavLink to="/">
+          <img src={require("../../images/logo.png")} alt="logo" />
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to="/">
+            <h3>Home</h3>
+          </NavLink>
+          <NavLink to="/about">
+            <h3>About</h3>
+          </NavLink>
+          <NavLink to="/products">
+            <h3>Products</h3>
+          </NavLink>
+          <NavLink to="/contact">
+            <h3>Contact</h3>
+          </NavLink>
+        </NavMenu>
+      </Nav>
+    </>
   );
 };
 
-export default Navbar
+export default Navbar;
