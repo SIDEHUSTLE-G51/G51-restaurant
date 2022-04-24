c00d7e788b2f4a174bacf24821d213080b29e38f
designeralabi <bdlkrmalabi@gmail.com>
2022-04-24 01:29:09 +0100

    created card component

---
+++ src/ProductsPage/Card.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const card = () => {
+    return <div>
+      <h3>Spicy food</h3>
+  </div>;
+};
+
+export default card;
--- src/ProductsPage/Products.jsx
+++ src/ProductsPage/Products.jsx
@@ -1,1 +1,12 @@
 import React from "react";
+import Card from "../ProductsPage/Card";
+
+const product = (params) => {
+  return (
+    <div>
+      <Card />
+    </div>
+  );
+};
+
+export default product;
