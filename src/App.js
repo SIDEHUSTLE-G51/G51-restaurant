24fee7b9ff98b167039c9b37d4aada63a6b2a68b
Nicholas Markus <nicxzmiller2@gmail.com>
2022-04-25 10:27:36 +0200

    Navbar Changes
    Made some changes to the navbar due to react-router-dom v6 principle

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "./App.css";
-import Navbar from "./components/Navbar";
-import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import About from "./AboutPage/About";
+import Products from "./ProductsPage/Products"
+import Contact from "./ContactPage/Contact";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Routes>
+        <Route path="/about" element={<About/>} exact />
+        <Route path="/products" element={<Products/>} exact />
+        <Route path="/contact" element={<Contact/>} exact />
+      </Routes>
     </Router>
   );
 }
--- src/ProductsPage/Products.jsx
+++ src/ProductsPage/Products.jsx
@@ -1,1 +1,9 @@
-import React from "react";
+import React from 'react'
+
+const Products = () => {
+  return (
+    <div>Products</div>
+  )
+}
+
+export default Products
