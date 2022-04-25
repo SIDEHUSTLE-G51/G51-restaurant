7b1d7a063c29c2d64d534422b12e5472cecac51d
Nicholas Markus <nicxzmiller2@gmail.com>
2022-04-25 19:12:33 +0200

    Footer
    Footer section

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import Navbar from "./components/Navbar/Navbar";
 import About from "./AboutPage/About";
 import Products from "./ProductsPage/Products"
 import Contact from "./ContactPage/Contact";
+import Home from "./HomePage/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Home/>} exact />
         <Route path="/about" element={<About/>} exact />
         <Route path="/products" element={<Products/>} exact />
         <Route path="/contact" element={<Contact/>} exact />
       </Routes>
+      <Footer />
     </Router>
   );
 }
