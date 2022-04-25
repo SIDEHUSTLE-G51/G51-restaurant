e372c7641b51ee6ded3f9b21821dc904ba59113b
Haru-hue <53390409+Haru-hue@users.noreply.github.com>
2022-04-25 11:18:40 +0100

    Updated Products.jsx
    Added the data file and mapped it into Products file, all that is needed is the CSS

---
+++ src/ProductsPage/components/Card.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Card (props) {
+    const { image, title, price } = props
+    return (
+        <div className="card">
+            <div className="card-image">
+                <img src={`../../images/${image}`} alt={title}/>
+            </div>
+            <div className="card-body">
+                {title}
+                <article>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis pellentesque ultricies consectetur purus tempor tincidunt ut dignissim. praesent placerat felis. Faucibus vitae auctor consectetur.
+                </article>
+                <h4 style={{color: "red"}}>Ingredients</h4>
+                <article>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis pellentesque ultricies consectetur purus tempor tincidunt ut dignissim. Augue ut vitae purus, praesent malesuada ultrices lacus, mattis. Tristique scelerisque amet, in id. Habitant dui praesent placerat felis. Faucibus vitae auctor consectetur.
+                </article>
+
+                <div className="order-section">
+                    <button className="order-button">Order Now</button>
+                    <input type="number" placeholder="0"></input>
+                </div>
+
+                <div className="price">
+                    <h3>{price}</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/ProductsPage/components/data.js
@@ -0,0 +1,63 @@
+const data = {
+    "superData": [
+        {
+            id: 1,
+            image: "",
+            title: "Spicy Yam + Fish Sauce",
+            price: "1,500"
+        },
+        {
+            id: 2,
+            image: "",
+            title: "Jollof rice + Chicken with Drink",
+            price: "1,700"
+        },
+        {
+            id: 3,
+            image: "",
+            title: "Fried Rice (Per Spoons)",
+            price: "1,900"
+        }
+    ],
+    "products": [
+        {
+            id: 4,
+            image: "",
+            title: "French Fries (Chips)",
+            price: "1,500"
+        },
+        {
+            id: 5,
+            image: "",
+            title: "Tantis Fish Ola",
+            price: "1,700"
+        },
+        {
+            id: 6,
+            image: "",
+            title: "White Rice (per Spoons)",
+            price: "200"
+        },
+        {
+            id: 7,
+            image: "",
+            title: "Jollof Rice (per Spoons)",
+            price: "1,500"
+        },
+        {
+            id: 8,
+            image: "",
+            title: "Fried Rice (per Spoons)",
+            price: "1,700"
+        },
+        {
+            image: "",
+            title: "Coconut Rice (per Spoons)",
+            price: "200"
+        },
+    ]
+}
+
+
+
+export default data
