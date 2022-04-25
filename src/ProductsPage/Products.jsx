c20007406430ec86f06a500bfe0a2a36c7f4a3c8
designeralabi <bdlkrmalabi@gmail.com>
2022-04-25 16:25:17 +0100

    restructured image folder to each components

--- src/ProductsPage/Products.jsx
+++ src/ProductsPage/Products.jsx
@@ -3,26 +3,25 @@ import data from "./components/data";
 import Card from "./components/Card";
 
 const Products = () => {
-    console.log(data)
-    const superSaver = data["superData"].map(item => {
-        return <Card title={item.title} price={item.price} key={item.id}/>
-    })
+  console.log(data);
+  const superSaver = data["superData"].map((item) => {
+    return <Card title={item.title} price={item.price} key={item.id} />;
+  });
 
-    const popularMeals = data["products"].map(item => {
-        return <Card title={item.title} price={item.price} key={item.id}/>
-    })
+  const popularMeals = data["products"].map((item) => {
+    return <Card title={item.title} price={item.price} key={item.id} />;
+  });
 
-    return (
-        
-        <section className="products">
-            <h2 className="product-title">Super Saver</h2>
-            <hr/>
-            {superSaver}
-            <h2 className="product-title">Popular Meals</h2>
-            <hr/>
-            {popularMeals}
-        </section>
-    )
-}
+  return (
+    <section className="products">
+      <h2 className="product-title">Super Saver</h2>
+      <hr />
+      {superSaver}
+      <h2 className="product-title">Popular Meals</h2>
+      <hr />
+      {popularMeals}
+    </section>
+  );
+};
 
-export default Products
+export default Products;
