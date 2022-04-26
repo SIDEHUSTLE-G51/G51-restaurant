cb093433b509badeb56163176bf352081fe81c3a
designeralabi <bdlkrmalabi@gmail.com>
2022-04-26 18:55:13 +0100

    create card.css & delete productCard style from app.js

--- src/ProductsPage/components/Card.jsx
+++ src/ProductsPage/components/Card.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import "./card.css";
 export default function Card(props) {
   const { image, title, price } = props;
   return (
     <div className="card">
       <div className="card-image">
-        <img src={image} alt={title} />
+        <img src={image} alt={title} className="card-img"/>
       </div>
       <div className="card-body">
         {title}
